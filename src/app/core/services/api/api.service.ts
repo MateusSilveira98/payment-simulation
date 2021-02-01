2d3af8fb0039f8814452ac8ff48d5dad567c171c
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-01 15:34:30 -0300

    feat: add api service unit test

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -1,4 +1,4 @@
-import { API } from './../API';
+import { API } from '../../API';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
