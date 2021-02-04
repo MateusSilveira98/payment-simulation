314eace1d98f0c4f91347d193827d5e85bc2d217
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-04 08:08:57 -0300

    chore: add transaction form modal unit test

--- src/app/shared/components/transaction-form-modal/transaction-form-modal.component.spec.ts
+++ src/app/shared/components/transaction-form-modal/transaction-form-modal.component.spec.ts
@@ -1,84 +1,104 @@
-import { Card } from './../../../core/domain/card/Card.domain';
 import { DebugElement } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed
+} from '@angular/core/testing';
 import { MatDialogRef } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormControlValidatorService } from '../../services/form-control-validator/form-control-validator.service';
+import { By } from '@angular/platform-browser';
+import { FormControlValidatorService } from './../../services/form-control-validator/form-control-validator.service';
 import { SharedModule } from './../../shared.module';
 import {
   TransactionForm,
-  TransactionFormModalComponent,
+  TransactionFormModalComponent
 } from './transaction-form-modal.component';
+import { MOCK_TRANSACTION_FORM_DATA } from './transaction-form.mock';
 
-const cards: Card[] = [
-  // valid card
-  {
-    card_number: '1111111111111111',
-    cvv: 789,
-    expiry_date: '01/18',
-  },
-  // invalid card
-  {
-    card_number: '4111111111111234',
-    cvv: 123,
-    expiry_date: '01/20',
-  },
-];
 describe('TransactionFormModalComponent', () => {
   let component: TransactionFormModalComponent;
   let fixture: ComponentFixture<TransactionFormModalComponent>;
-  let element: DebugElement;
+  let debugElement: DebugElement;
 
-  let formBuilder: any;
-  let validatorMessageService: any;
-  let dialogRef: any;
-
-  const data: TransactionForm = {
-    cards,
-    card_number: '1111111111111111',
-    value: '123',
-    user: {
-      id: 1,
-      img: 'url',
-      name: 'mock',
-      username: 'mock@mock',
-    },
-  };
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<TransactionForm>>;
 
   beforeEach(async(() => {
-    const formBuilderSpy = jasmine.createSpyObj('FormBuilder', ['group']);
-    const validatorMessageServiceSpy = jasmine.createSpyObj(
-      'FormControlValidatorMessageService',
-      ['findError']
-    );
-    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close', 'afterClosed']);
     TestBed.configureTestingModule({
       imports: [SharedModule],
       providers: [
-        { provide: FormBuilder, useValue: formBuilderSpy },
-        {
-          provide: FormControlValidatorService,
-          useValue: validatorMessageServiceSpy,
-        },
         { provide: MatDialogRef, useValue: dialogRefSpy },
-        { provide: MAT_DIALOG_DATA, useValue: data },
+        { provide: MAT_DIALOG_DATA, useValue: MOCK_TRANSACTION_FORM_DATA },
       ],
     })
       .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(TransactionFormModalComponent);
         component = fixture.componentInstance;
-        element = fixture.debugElement;
-        formBuilder = TestBed.get(FormBuilder);
-        validatorMessageService = TestBed.get(
-          FormControlValidatorService
-        );
-        dialogRef = TestBed.get(MatDialogRef);
+        debugElement = fixture.debugElement;
+        fixture.detectChanges();
       });
   }));
 
-  it('should create', () => {
+  it('should create transaction form modal component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should retrive the last four card numbers', () => {
+    const lastForNumbers = component.getLastFourNumbers(
+      MOCK_TRANSACTION_FORM_DATA.card_number
+    );
+    expect(lastForNumbers.length).toBe(4);
+  });
+
+  it('should build form with datas on initialize the transaction form modal component', fakeAsync(() => {
+    expect(component.transactionForm.get('value')).toBeTruthy();
+    expect(component.transactionForm.get('card_number')).toBeTruthy();
+
+    expect(component.transactionForm.get('value').value).toBe(
+      MOCK_TRANSACTION_FORM_DATA.value
+    );
+    expect(component.transactionForm.get('card_number').value).toBe(
+      MOCK_TRANSACTION_FORM_DATA.card_number
+    );
+
+    const inputValue = debugElement.queryAll(By.css('input'));
+    expect(inputValue[0].attributes.formControlName).toEqual('value');
+
+    expect(
+      inputValue[0].nativeNode.value.replace('R$ ', '').replace(',', '.')
+    ).toContain(MOCK_TRANSACTION_FORM_DATA.value);
+
+    fixture.detectChanges();
+
+    const selectInputCardNumber = debugElement.queryAll(
+      By.css('.mat-select-value-text span')
+    );
+    const lastForNumbers = component.getLastFourNumbers(
+      MOCK_TRANSACTION_FORM_DATA.card_number
+    );
+
+    expect(selectInputCardNumber[0].nativeElement.textContent).toContain(
+      `com final ${lastForNumbers}`
+    );
+  }));
+
+  it('should retrive a message error on validate form fields', () => {
+    const formControlValidatorService = new FormControlValidatorService();
+
+    const valueControl = component.transactionForm.get('value');
+    const cardNumberControl = component.transactionForm.get('card_number');
+
+    expect(valueControl).toBeTruthy();
+    expect(valueControl.invalid).toBeTruthy();
+
+    expect(cardNumberControl).toBeTruthy();
+    expect(cardNumberControl.valid).toBeTruthy();
+
+    const valueFieldMessage = component.getErrorMessage(valueControl, 'Valor');
+    expect(valueFieldMessage).toContain(
+      formControlValidatorService.getMinLengthErrorMessage(valueControl.errors.min.min, 'Valor')
+    );
   });
 });
